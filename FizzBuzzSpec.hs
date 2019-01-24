0dd72bb8371af751925beead60ddf0a2ca10cac2
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-24 15:22:00 +0000

    Write divisible by three test

--- FizzBuzzSpec.hs
+++ FizzBuzzSpec.hs
@@ -8,3 +8,6 @@ main = hspec $ do
   describe "FizzBuzz" $ do
     it "returns numbers not divisible by 3 or 5" $
       fizzbuzz 1 `shouldBe` 1
+
+    it "returns 'Fizz' if number divides by 3" $
+      fizzbuzz 3 `shouldBe` "Fizz"
