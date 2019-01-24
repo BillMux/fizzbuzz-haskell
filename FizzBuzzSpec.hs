de7d27938aba4fc7447f5672e20c7a490cb278c9
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-24 15:29:18 +0000

    Change divisible by three test to expect str output

--- FizzBuzzSpec.hs
+++ FizzBuzzSpec.hs
@@ -7,7 +7,7 @@ main :: IO ()
 main = hspec $ do
   describe "FizzBuzz" $ do
     it "returns numbers not divisible by 3 or 5" $
-      fizzbuzz 1 `shouldBe` 1
+      fizzbuzz 1 `shouldBe` "1"
 
     it "returns 'Fizz' if number divides by 3" $
       fizzbuzz 3 `shouldBe` "Fizz"
