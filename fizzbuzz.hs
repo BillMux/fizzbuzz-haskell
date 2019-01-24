ace2978d4d5733d1901cc21de3280a61c6ca9e79
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-24 16:04:48 +0000

    Delete notes

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,8 +1,6 @@
 module FizzBuzz where
 
 fizzbuzz :: Int -> [Char]
---  Char  -> single character           e.g. 'a' (expressed with single quotes)
--- [Char] -> array of single characters e.g. "Bill" (expressed with double quotes)
 
 fizzbuzz n = if n `rem` 15 == 0 then "FizzBuzz"
   else if n `rem` 3 == 0 then "Fizz"
