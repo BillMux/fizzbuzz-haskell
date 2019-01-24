7815ce1e225db57b23fcce7bbc8b5a2b055fdfa6
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-24 15:32:54 +0000

    Pass divisible by five Buzz test

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -4,4 +4,6 @@ fizzbuzz :: Int -> [Char]
 --  Char  -> single character           e.g. 'a' (expressed with single quotes)
 -- [Char] -> array of single characters e.g. "Bill" (expressed with double quotes)
 
-fizzbuzz n = if n `rem` 3 == 0 then "Fizz" else show n :: [Char]
+fizzbuzz n = if n `rem` 3 == 0 then "Fizz"
+  else if n `rem` 5 == 0 then "Buzz"
+  else show n :: [Char]
