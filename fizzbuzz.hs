eb10c444c391e334e469acf224c099760438567e
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-24 15:28:46 +0000

    Pass divisible by three Fizz test

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -4,4 +4,4 @@ fizzbuzz :: Int -> [Char]
 --  Char  -> single character           e.g. 'a' (expressed with single quotes)
 -- [Char] -> array of single characters e.g. "Bill" (expressed with double quotes)
 
-fizzbuzz n = if n `rem` 3 == 0 then "Fizz" else show n
+fizzbuzz n = if n `rem` 3 == 0 then "Fizz" else show n :: [Char]
