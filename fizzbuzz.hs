f470eb7013cf9b14fa20b8f7577767c2ea21245b
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-24 15:22:29 +0000

    Divisible by three function started, not complete

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,5 +1,7 @@
 module FizzBuzz where
 
-fizzbuzz :: Int -> Int
+fizzbuzz :: Int -> [Char]
+--  Char  -> single character           e.g. 'a' (expressed with single quotes)
+-- [Char] -> array of single characters e.g. "Bill" (expressed with double quotes)
 
-fizzbuzz n = n
+fizzbuzz n = if n `rem` 3 == 0 then "Fizz" else show n
