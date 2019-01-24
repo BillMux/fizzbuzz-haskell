dd5cc1b589f4c1a1a7372012019fdc7e68ca2f1f
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-24 15:42:39 +0000

    FizzBuzz when divides by 3 and 5

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -4,6 +4,7 @@ fizzbuzz :: Int -> [Char]
 --  Char  -> single character           e.g. 'a' (expressed with single quotes)
 -- [Char] -> array of single characters e.g. "Bill" (expressed with double quotes)
 
-fizzbuzz n = if n `rem` 3 == 0 then "Fizz"
+fizzbuzz n = if n `rem` 15 == 0 then "FizzBuzz"
+  else if n `rem` 3 == 0 then "Fizz"
   else if n `rem` 5 == 0 then "Buzz"
   else show n :: [Char]
