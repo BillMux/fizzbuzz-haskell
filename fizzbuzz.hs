113b1f8bd0c8db158697bab991ea0a8610a1e5d9
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-25 12:35:12 +0000

    Refactor using case block

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -2,7 +2,7 @@ module FizzBuzz where
 
 fizzbuzz :: Int -> [Char]
 
-fizzbuzz n = case (n `rem` 3 , n `rem` 5) of
+fizzbuzz n = case (n `rem` 3, n `rem` 5) of
   (0,0) -> "FizzBuzz"
   (0,_) -> "Fizz"
   (_,0) -> "Buzz"
